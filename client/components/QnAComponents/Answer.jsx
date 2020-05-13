2f198ca9bafd4664a8b1927e5c762cecc324a626
maxwellrk <maxkatz23@gmail.com>
2020-05-13 17:05:35 -0400

    Added rough outline for Answer.jsx

--- client/components/QnAComponents/Answer.jsx
+++ client/components/QnAComponents/Answer.jsx
@@ -1,7 +1,21 @@
 const React = require('react');
 
-const Answer = (props) => {
-  return <div>Answer</div>;
+const Answer = ({ info }) => {
+  return (
+    <div>
+      <h3>A:</h3>
+      <text>User: </text>
+      <text>{info.answer_name}</text>
+      <br />
+      <text>Date: </text>
+      <text>{info.date}</text>
+      <br />
+      <text>Answer: </text>
+      <text>{info.body}</text>
+      <br />
+      <text>Helpful? Yes({info.helpfulness})</text>
+    </div>
+  );
 };
 
 export default Answer;
