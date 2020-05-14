047be2fb3bc5279a2430431544c58d5050fdc275
Austin Douse <douse.austin@gmail.com>
2020-05-14 14:58:09 -0400

    add carousel displaying photos in one frame

--- client/components/Overview/Carousel.jsx
+++ client/components/Overview/Carousel.jsx
@@ -1,8 +1,54 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import CarouselContent from "./CarouselContent";
+import Slide from "./Slide";
 
-const Carousel = (props) => {
-  console.log("props in carousel", props);
-  return <div>Carousel</div>;
+/**
+ * @function Slider
+ */
+const Carousel = ({ currentStyle }) => {
+  //   const getWidth = () => window.innerWidth;
+
+  const [state, setState] = useState({
+    translate: 0,
+    transition: 0.45,
+  });
+
+  const { translate, transition } = state;
+  console.log(currentStyle.photos);
+
+  return (
+    <div
+      style={{
+        height: 400,
+        width: 400,
+        position: "relative",
+        overflow: "hidden",
+        margin: 0,
+      }}
+    >
+      <CarouselContent
+        translate={translate}
+        transition={transition}
+        width={400}
+        height={400}
+        overflow="hidden"
+      >
+        {currentStyle.photos
+          ? currentStyle.photos.map((slide, index) => (
+              <Slide key={index} content={slide.thumbnail_url} />
+            ))
+          : ""}
+      </CarouselContent>
+    </div>
+  );
 };
+
+// const CarouselCSS = css`
+//   position: relative;
+//   height: 400;
+//   width: 400;
+//   margin: 0 auto;
+//   overflow: hidden;
+// `;
 
 export default Carousel;
