edfbb786319d111fb8737e73150353f0d26b7577
maxwellrk <maxkatz23@gmail.com>
2020-05-14 15:33:38 -0400

    Added search bar functionality (will preform searches on matching) body content (if search has 3+ chars)

--- client/components/QnAComponents/QnA.jsx
+++ client/components/QnAComponents/QnA.jsx
@@ -1,16 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import QnABlock from './QnABlock';
-import SearchBar from './QuestionBar';
+import SearchBar from './SearchBar';
 
 const QnA = ({ fetchQuestionsById, questionsList, productById }) => {
   const [renderAmount, changeRenderAmount] = useState(2);
   const [searchInput, changeSearchInput] = useState('');
 
   useEffect(() => {
-    fetchQuestionsById(productById.id).then(() => {
-      return changeRenderAmount(2);
-    });
+    fetchQuestionsById(productById.id)
+      .then(() => {
+        return changeRenderAmount(2);
+      })
+      .then(() => {
+        changeSearchInput('');
+      });
   }, [productById]);
+
+  //filter function for searchBar
+  const filterQuestions = (entry) => {
+    //this function is currently forcing a requery of questions even
+    //if its under 3 characters, should come back to make it only query if over 3
+    //maybe make a function that creates filter callbacks?
+
+    //changing of the query will not reset the renderAmount, maybe it should?
+    let query = searchInput.length > 2 ? searchInput.toLowerCase() : '';
+    return entry.question_body.toLowerCase().indexOf(query) > -1;
+  };
+
+  //more questions button rendering function
 
   let button;
 
@@ -34,12 +51,17 @@ const QnA = ({ fetchQuestionsById, questionsList, productById }) => {
         border: '2px solid black',
       }}
     >
+      <SearchBar
+        searchInput={searchInput}
+        changeSearchInput={changeSearchInput}
+      />
       {/* should find a better way to do this, it's going to need to sort the list
           everytime, this sort should be done after productById changes, and keep a local state
           of the sorted array
        */}
       {Object.keys(questionsList).length ? (
         questionsList.results
+          .filter(filterQuestions)
           .sort((a, b) => {
             //sort by descending helpfulness
             return b.question_helpfulness - a.question_helpfulness;
--- client/components/QnAComponents/SearchBar.jsx
+++ client/components/QnAComponents/SearchBar.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
-const SearchBar = (props) => {
-  return <div>SearchBar</div>;
+const SearchBar = ({ searchInput, changeSearchInput }) => {
+  return (
+    <form
+      onSubmit={() => {
+        event.preventDefault();
+      }}
+    >
+      <input
+        type="“Have a question? Search for answers…”"
+        value={searchInput}
+        onChange={(e) => {
+          changeSearchInput(e.target.value);
+          console.log(searchInput);
+        }}
+      />
+    </form>
+  );
 };
 
 export default SearchBar;
