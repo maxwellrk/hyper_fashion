ae5b1e5a23e0d1e7f6971253960380520b40dc45
Austin Douse <douse.austin@gmail.com>
2020-05-14 15:43:17 -0400

    add basic arrow functionality

--- client/components/Overview/Carousel.jsx
+++ client/components/Overview/Carousel.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import CarouselContent from "./CarouselContent";
 import Slide from "./Slide";
+import Arrow from "./Arrows";
 
 /**
  * @function Slider
@@ -9,36 +10,74 @@ const Carousel = ({ currentStyle }) => {
   //   const getWidth = () => window.innerWidth;
 
   const [state, setState] = useState({
+    index: 0,
     translate: 0,
     transition: 0.45,
   });
 
-  const { translate, transition } = state;
-  console.log(currentStyle.photos);
+  const { translate, transition, index } = state;
+
+  const nextSlide = () => {
+    if (index === currentStyle.photos.length - 1) {
+      return setState({
+        ...state,
+        translate: 0,
+        index: 0,
+      });
+    }
+
+    setState({
+      ...state,
+      index: index + 1,
+      translate: (index + 1) * 600,
+    });
+  };
+
+  const prevSlide = () => {
+    if (index === 0) {
+      return setState({
+        ...state,
+        translate: (currentStyle.photos.length - 1) * 600,
+        index: currentStyle.photos.length - 1,
+      });
+    }
+
+    setState({
+      ...state,
+      index: index - 1,
+      translate: (index - 1) * 600,
+    });
+  };
 
   return (
     <div
       style={{
         height: 400,
-        width: 400,
+        width: 600,
         position: "relative",
         overflow: "hidden",
         margin: 0,
       }}
     >
-      <CarouselContent
-        translate={translate}
-        transition={transition}
-        width={400}
-        height={400}
-        overflow="hidden"
-      >
-        {currentStyle.photos
-          ? currentStyle.photos.map((slide, index) => (
-              <Slide key={index} content={slide.thumbnail_url} />
-            ))
-          : ""}
-      </CarouselContent>
+      {currentStyle.photos ? (
+        <CarouselContent
+          translate={translate}
+          transition={transition}
+          width={600 * currentStyle.photos.length}
+          height={600}
+          overflow="hidden"
+        >
+          {currentStyle.photos
+            ? currentStyle.photos.map((slide, index) => (
+                <Slide key={index} content={slide.thumbnail_url} />
+              ))
+            : ""}
+        </CarouselContent>
+      ) : (
+        ""
+      )}
+      <Arrow direction="left" handleClick={prevSlide} />
+      <Arrow direction="right" handleClick={nextSlide} />
     </div>
   );
 };
