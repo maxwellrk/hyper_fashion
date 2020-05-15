71b1859361d147197162183dfae090e377dd70b1
maxwellrk <maxkatz23@gmail.com>
2020-05-15 11:24:15 -0400

    Answers from "Seller" will render in bold (current just conditional logic in)

--- client/components/QnAComponents/Answer.jsx
+++ client/components/QnAComponents/Answer.jsx
@@ -7,8 +7,11 @@ const Answer = ({ info }) => {
       <p>{info.body}</p>
 
       <p>
-        by {info.answerer_name}, {info.newDate} | Helpful? Yes (
-        {info.helpfulness}) | Report
+        by{' '}
+        {info.answerer_name === 'Seller'
+          ? info.answerer_name + '(BOLD)'
+          : info.answerer_name}
+        , {info.newDate} | Helpful? Yes ({info.helpfulness}) | Report
       </p>
     </div>
   );
