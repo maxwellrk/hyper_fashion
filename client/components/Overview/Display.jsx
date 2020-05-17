0b96c186880544f4d6d02b059a1e64971c7232fd
Austin Douse <douse.austin@gmail.com>
2020-05-17 11:38:12 -0400

    add sales price functionality

--- client/components/Overview/Display.jsx
+++ client/components/Overview/Display.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
 const Display = ({currentStyle, productById}) => {
+  console.log("current style in display", currentStyle);
   return (
     <div>
       <div>{productById.category}</div>
       <h1>{productById.name}</h1>
-      <div>${currentStyle.original_price}</div>
+      <div>
+        {currentStyle.sale_price > 0
+          ? "SALE PRICE $" + currentStyle.sale_price
+          : "$" + currentStyle.original_price}
+      </div>
       <br />
     </div>
   );
