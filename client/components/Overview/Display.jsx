195fa292a31b76aed16027aa1bcfc1a2ed7c27d8
Austin Douse <douse.austin@gmail.com>
2020-05-17 10:48:30 -0400

    just committing

--- client/components/Overview/Display.jsx
+++ client/components/Overview/Display.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Display = ({ currentStyle, productById }) => {
+const Display = ({currentStyle, productById}) => {
   return (
     <div>
       <div>{productById.category}</div>
