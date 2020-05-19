8668902b340bbe90dc30e504cf4e24c13cb04d20
Austin Douse <douse.austin@gmail.com>
2020-05-19 18:56:05 -0400

    add the start of tests

--- tests/OverviewTests/Overview.test.js
+++ tests/OverviewTests/Overview.test.js
@@ -1,6 +1,34 @@
+import React from "react";
+import {shallow} from "enzyme";
+import store from "../../client/store/store";
+import Overview from "../../client/containers/OverviewContainers/OverviewContainer";
+import ProductDescription from "../../client/containers/OverviewContainers/productDescriptionContainer";
+import Carousel from "../../client//containers/OverviewContainers/CarouselContainer";
+
 describe("Overview", () => {
   it("should be true", () => {
     const truthy = true;
     expect(truthy).toBe(true);
+  });
+});
+
+describe("Overview tests", () => {
+  test("renders", () => {
+    const wrapper = shallow(<Overview store={store} />);
+    expect(wrapper.exists()).toBe(true);
+  });
+});
+
+describe("Product Description tests", () => {
+  test("renders", () => {
+    const wrapper = shallow(<ProductDescription store={store} />);
+    expect(wrapper.exists()).toBe(true);
+  });
+});
+
+describe("Product Description tests", () => {
+  test("renders", () => {
+    const wrapper = shallow(<Carousel store={store} />);
+    expect(wrapper.exists()).toBe(true);
   });
 });
